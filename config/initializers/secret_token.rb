7b5f7d6c3e708c6f10bdee12e7c5fb8e0de08465
Marlon Gomes Lopes <marlonglopes@gmail.com>
2010-11-22 14:38:54 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pop::Application.config.secret_token = 'a113b464be8de9b9f6dc7c7f57c9a54605fb22dea06f1c283b39f42dbb3a1d8ce5dcef81ff9f35953091023982aefac5f1126478983b79edbf5a3174a83babf6'
